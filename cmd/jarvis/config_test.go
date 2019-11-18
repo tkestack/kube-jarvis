ba4710f887a3620befb86be4e209310b25c42892
raylhuang <rayhuang110@gmail.com>
2019-11-18 12:42:04 +0800

    Test only for pkg

--- cmd/jarvis/config_test.go
+++ cmd/jarvis/config_test.go
@@ -7,7 +7,37 @@ import (
 )
 
 func TestGetConfig(t *testing.T) {
-	c, err := GetConfig("default.yaml", logger.NewLogger())
+	data := `
+global:
+  cluster:
+    kubeconfig: "fake"
+coordinate:
+  type: "default"
+# config:
+#   xxx: "123"
+
+diagnostics:
+  - type: "example"
+    name: "example 1"
+    score: 10
+    weight: 10
+    config:
+      message: "this is a example diagnostic"
+
+evaluators:
+  - type: "sum"
+    name: "sum 1"
+    # config:
+    #   xxx: "123"
+
+exporters:
+  - type: "stdout"
+    name: "stdout 1"
+    # config:
+    #   xxx: "123"
+`
+
+	c, err := getConfig([]byte(data), logger.NewLogger())
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
