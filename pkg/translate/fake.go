e24d02bb79f72b4d0304803c9b7ce7467c01d5a8
raylhuang <rayhuang110@gmail.com>
2019-11-19 09:25:09 +0800

    Add comment

--- pkg/translate/fake.go
+++ pkg/translate/fake.go
@@ -1,16 +1,12 @@
 package translate
 
+// Fake is a Translator that just return ID as message directly
 type Fake struct {
 }
 
+// NewFake return a Fake Translator
 func NewFake() Translator {
 	return &Fake{}
-}
-
-// WithModule attach a module label to a Translator
-// module will be add before ID when you call Translator.Message
-func (f *Fake) Message(ID string, templateData map[string]interface{}) Message {
-	return Message(ID)
 }
 
 // Message get translated message from Translator
@@ -18,6 +14,12 @@ func (f *Fake) Message(ID string, templateData map[string]interface{}) Message {
 // example:
 //         ID = "message"  and module = "diagnostics.example"
 //         then real ID will be "diagnostics.example.message"
+func (f *Fake) Message(ID string, templateData map[string]interface{}) Message {
+	return Message(ID)
+}
+
+// WithModule attach a module label to a Translator
+// module will be add before ID when you call Translator.Message
 func (f *Fake) WithModule(module string) Translator {
 	return f
 }
