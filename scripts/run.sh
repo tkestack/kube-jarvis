6ec115659137bd9f0392c245aa1d0e87522eacff
Your Name <you@example.com>
2019-12-06 11:31:08 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,19 @@
 echo "Download kube-jarvis...."
 wget https://kube-jarvis-1251707795.cos.ap-guangzhou.myqcloud.com/kube-jarvis.tar.gz
 tar xf kube-jarvis.tar.gz
-cd  kube-jarvis
+echo "Creating Namespace kube-jarvis..."
+kubectl apply -f manifests/common/namespace.yaml
+echo "Creating Daemonset kube-jarvis-agent..."
+kubectl apply -f manifests/common/agent-ds.yaml
+while true
+do
+     echo "Waiting all kube-jarvis-agent to running..."
+     check=`kubectl get po -n kube-jarvis | grep -v NAME | grep -v Running | grep -v grep`
+     if [ "$check" == "" ]
+     then
+             echo "Done"
+             break
+     fi
+     sleep 3
+done
 ./kube-jarvis
